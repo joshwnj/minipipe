7738ded8373b73b24ecfca13ed995fe0e19b9551
Josh Johnston <josh@x-team.com>
2014-12-05 13:48:02 +1100

    initial commit

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ Traditionally we do this with streams.
 In this experiment, we're asking the question _"What if you are cool with the limitation of each pipe only being able to connect to one thing?"_, and seeing if there is anything of value here which is not too crazy or stupid.
 
 The idea here is that objects can provide the minimal interface for these 1-to-1 pipes, and then if you ever need more it should be trivial to pipe into a regular stream (or eventemitter even).
+
+License
+----
+
+MIT
---
+++ index.js
@@ -0,0 +1,40 @@
+function Minipipe (receiveFunc) {
+  this._receiveFunc = receiveFunc;
+  this._targetFunc = null;
+}
+
+Minipipe.prototype.emit = function (data) {
+  if (this._targetFunc) {
+    this._targetFunc(data);
+  }
+};
+
+Minipipe.prototype.receive = function (data) {
+  if (this._receiveFunc) {
+    this._receiveFunc(data);
+  }
+};
+
+Minipipe.prototype.to = function (targetPipe) {
+  if (typeof targetPipe === 'function') {
+    this._targetFunc = targetPipe;
+  }
+
+  else if (typeof targetPipe.receive === 'function') {
+    this._targetFunc = targetPipe.receive.bind(targetPipe);
+  }
+
+  return targetPipe;
+};
+
+Minipipe.prototype.through = function (func) {
+  var pipe = new Minipipe(function (data) {
+    pipe.emit(func(data));
+  });
+
+  this.to(pipe);
+
+  return pipe;
+};
+
+module.exports = Minipipe;
