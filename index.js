74db2ca05db37ca2f91ca2a676d068d809dcce0e
Josh Johnston <josh@x-team.com>
2014-12-06 13:33:34 +1100

    added .peek shortcut to create pipes out of functions that emit the original value without changing it.

--- index.js
+++ index.js
@@ -31,9 +31,36 @@ Minipipe.prototype.to = function (targetPipe) {
   return targetPipe;
 };
 
+/*
+
+  Shortcut to create a minipipe out of a function.
+  The return value of the function will be the emitted value of the new pipe.
+
+*/
 Minipipe.prototype.through = function (func) {
   var pipe = new Minipipe(function (data) {
     pipe.emit(func(data));
+  });
+
+  this.to(pipe);
+
+  return pipe;
+};
+
+/*
+
+  Similar to `.through`, except the data will always be passed on untouched
+  (So you can plug in functions that receive the data without worrying about their return value)
+
+*/
+Minipipe.prototype.peek = function (func) {
+  var wrapper = function (data) {
+    func(data);
+    return data;
+  };
+
+  var pipe = new Minipipe(function (data) {
+    pipe.emit(wrapper(data));
   });
 
   this.to(pipe);
